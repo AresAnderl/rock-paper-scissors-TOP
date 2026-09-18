f65fecf032922a87709c4c76d2a3ef417606d219
Alex <hp.val84@gmail.com>
2026-09-18 15:08:20 -0500

    Add game UI and reworked code

--- rock.js
+++ rock.js
@@ -1,48 +1,137 @@
 // implementation:
 
-// Prompt the user to type in a number. store the result and compare ir later on with the value random generated
-const humanChoose = Number(
-  prompt(`What do you choose (please, only write the number): 
-    1:Rock
-    2:Paper
-    3:Scrissors`),
-);
+// all selectors:
+let playerWins = 0;
+let computerWins = 0;
+let ties = 0;
 
-// use randomness to generate a number between 1 and 3 for the 3 options
-const randomNumber = Math.ceil(Math.random() * 3);
-let computerChoose =
-  randomNumber === 1
+const playerWinHTML = document.querySelector(".player--count");
+const computerWinHTML = document.querySelector(".computer--count");
+const tieHTML = document.querySelector(".tie--count");
+
+const playButtons = document.querySelector(".play--game");
+const winnerCongrats = document.querySelector(".winner--congrats");
+
+// put name to computrer selection
+const getRPS = (number) => {
+  return number === 1
     ? "rock"
-    : false || randomNumber === 2
+    : number === 2
       ? "paper"
-      : false || randomNumber === 3
+      : number === 3
         ? "scissors"
         : false;
+};
 
-// log to the console the selections to make it easier to debug
-
-console.log(randomNumber, computerChoose, humanChoose);
-
-// if else statements to create the function to determine who wins
-const compareResults = function () {
-  if (humanChoose === randomNumber) {
-    console.log(`It is a tie. you both choose ${computerChoose}`);
-    return;
+const whoWins = (player, computer) => {
+  // tie case
+  if (player === computer) {
+    ties++;
+    return "tie";
   }
-
-  if (randomNumber === 1) {
-    console.log(
-      `${humanChoose === 2 ? "You win: you chose paper and machine chose " + computerChoose : "You lose: you chose scissors and machine chose " + computerChoose}`,
-    );
-  } else if (randomNumber === 2) {
-    console.log(
-      `${humanChoose === 3 ? "You win: you chose scissors and machine chose " + computerChoose : "You lose: you chose rock and machine chose " + computerChoose}`,
-    );
-  } else if (randomNumber === 3) {
-    console.log(
-      `${humanChoose === 1 ? "You win: you chose rock and machine chose " + computerChoose : "You lose: you chose paper and machine chose " + computerChoose}`,
-    );
+  // rock case
+  if (player === "rock") {
+    if (computer === "scissors") {
+      playerWins++;
+      return "player wins";
+    } else {
+      computerWins++;
+      return "computer wins";
+    }
+  }
+  // paper case
+  if (player === "paper") {
+    if (computer === "rock") {
+      playerWins++;
+      return "player wins";
+    } else {
+      computerWins++;
+      return "computer wins";
+    }
+  }
+  // scissors case
+  if (player === "scissors") {
+    if (computer === "paper") {
+      playerWins++;
+      return "player wins";
+    }
+    {
+      computerWins++;
+      return "computer wins";
+    }
   }
 };
 
-compareResults();
+const sumWins = (winner) => {
+  if (winner === "tie") {
+    tieHTML.textContent = `ties: ${ties}`;
+  }
+  if (winner === "player wins") {
+    playerWinHTML.textContent = `player wins: ${playerWins}`;
+  }
+  if (winner === "computer wins") {
+    computerWinHTML.textContent = `computer wins: ${computerWins}`;
+  }
+};
+
+// use randomness to generate a number between 1 and 3 for the 3 options
+const computerTry = () => {
+  const randomNumber = Math.ceil(Math.random() * 3);
+  const computerChoose = getRPS(randomNumber);
+  return computerChoose;
+};
+
+// when user click button get their selection and play game
+const humanTry = (e) => {
+  const target = e.target;
+  console.log(target);
+  if (target.classList.contains("rock")) {
+    console.log("rock");
+    playGame("rock");
+  } else if (target.classList.contains("scissors")) {
+    console.log("scissors");
+    playGame("scissors");
+  } else if (target.classList.contains("paper")) {
+    console.log("paper");
+    playGame("paper");
+  }
+};
+
+const resetCounters = () => {
+  playerWins = 0;
+  playerWinHTML.textContent = "player wins: 0";
+  computerWins = 0;
+  computerWinHTML.textContent = "computer wins: 0";
+  ties = 0;
+  tieHTML.textContent = "ties: 0";
+};
+
+// check if game is finished
+const finishGame = () => {
+  if (playerWins === 5) {
+    winnerCongrats.textContent = "player has won the game!";
+    resetCounters();
+  }
+  if (computerWins === 5) {
+    winnerCongrats.textContent = "computer has won the game!";
+    resetCounters();
+  }
+};
+
+//play game
+const playGame = (player) => {
+  if (winnerCongrats.textContent.length > 1) {
+    winnerCongrats.textContent = " ";
+  }
+  const computer = computerTry();
+  const winner = whoWins(player, computer);
+  console.log(`computer chose: ${computer}. player chose: ${player}`);
+  sumWins(winner);
+  finishGame();
+};
+
+// select div element that contains the buttons
+
+console.log(playButtons);
+
+playButtons.addEventListener("click", humanTry);
